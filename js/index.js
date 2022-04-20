4ac8597535fe1d637066648c040159ee3bea4bb6
Mtutyunjay Madarimath <88146245+JayMadari@users.noreply.github.com>
2022-04-20 12:37:01 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Nav Menu Show Overlay
+
+const menuShowBtn = document.getElementById('menu-show-btn');
+const menuHideBtn = document.getElementById('menu-hide-btn');
+const menuOverlay = document.querySelector('.nav-overlay');
+
+menuShowBtn.addEventListener('click', () => menuOverlay.classList.add('show-overlay'));
+menuHideBtn.addEventListener('click', () => menuOverlay.classList.remove('show-overlay'));
+
